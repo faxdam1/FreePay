589ab30a2f575f0de847100cd29f3cd1e122d6f6
faxdam1 <faxdam1@gmail.com>
2014-08-20 22:32:09 -0500

    Desarrollo de vista consultarCobroView y validaciones.

--- FreePay/src/main/java/com/freepay/aplicacion/CobroBean.java
+++ FreePay/src/main/java/com/freepay/aplicacion/CobroBean.java
@@ -1,6 +1,23 @@
 package com.freepay.aplicacion;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller("CobroBean") 
+@ManagedBean(name = "CobroBean")
+@ViewScoped
 public class CobroBean {
 	
+	
+	 public void agregarCliente(ActionEvent actionEvent) {
+
+		 
+	 }
 	
 }
